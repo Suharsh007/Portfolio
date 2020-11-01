a08adfe4b01ad592e50eb0da8a5a990683095330
Suharsh007 <64641725+Suharsh007@users.noreply.github.com>
2020-11-01 23:20:35 +0530

    Initial Commit

---
+++ home/views.py
@@ -0,0 +1,29 @@
+from django.shortcuts import render, HttpResponse
+from home.models import Contact
+
+# Create your views here.
+def home(request):
+    # return HttpResponse("This is my HomePage (/)")
+    context = {'name': 'Suharsh' , 'course': 'Django'}
+    return render(request, 'index.html', context)
+
+def about(request):
+   #  return HttpResponse("This is my AboutPage (/about)")
+    return render(request, 'about.html')
+def projects(request):
+   #  return HttpResponse("This is my HomePage (/projects)")
+      return render(request, 'projects.html')
+
+def contact(request):
+  #   return HttpResponse("This is my ContactPage (/contact)")
+  if request.method =="POST":
+     
+      name = request.POST['name']
+      email = request.POST['email']
+      phone = request.POST['phone']
+      desc = request.POST['desc']
+  
+      ins = Contact(name =name, email =email, phone=phone, desc=desc)
+      ins.save()
+     
+  return render(request, 'contact.html')
